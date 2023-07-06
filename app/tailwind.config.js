4ce2034f071f0aae764178dbab63800f10cea7d7
entest <entest>
2023-07-06 08:22:46 +0000

    first

---
+++ app/tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}", "./templates/**/*.html"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
