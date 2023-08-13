597dfbce57f21de0e0fab8ac76d54778e78b6c59
entest <entest>
2023-08-13 12:59:24 +0700

    test

---
+++ app/build.py
@@ -0,0 +1,29 @@
+import os 
+
+# parameters
+REGION = "ap-southeast-1"
+ACCOUNT = "392194582387"
+
+# delete all docker images 
+os.system("sudo docker system prune -a") 
+
+# build flask-app image 
+os.system("sudo docker build -t flask-app . ")
+
+#  aws ecr login 
+os.system(f"aws ecr get-login-password --region {REGION} | sudo docker login --username AWS --password-stdin {ACCOUNT}.dkr.ecr.{REGION}.amazonaws.com")
+
+# get image id 
+IMAGE_ID=os.popen("sudo docker images -q flask-app:latest").read()
+
+# tag flask-app image 
+os.system(f"sudo docker tag {IMAGE_ID.strip()} {ACCOUNT}.dkr.ecr.{REGION}.amazonaws.com/flask-app:latest")
+
+# create ecr repository 
+try: 
+  os.system(f"aws ecr create-repository --registry-id {ACCOUNT} --repository-name flask-app")
+except:
+  print("already existed")
+
+# push image to ecr 
+os.system(f"sudo docker push {ACCOUNT}.dkr.ecr.{REGION}.amazonaws.com/flask-app:latest")
